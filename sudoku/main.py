080704d50aa153027ceaf6a192a168d50857745a
KalmeMarq <55203647+KalmeMarq@users.noreply.github.com>
2023-10-22 20:18:31 +0100

    font

--- sudoku/main.py
+++ sudoku/main.py
@@ -2,11 +2,15 @@ import sys
 from PySide6.QtCore import *
 from PySide6.QtWidgets import *
 from PySide6.QtGui import *
+from PySide6.QtWebEngineWidgets import *
 
 class ApplicationWindow(QMainWindow):
   def __init__(self):
     super().__init__()
     self.setWindowTitle("Sudoku")
+    self.webview = QWebEngineView()
+    self.webview.setUrl("https://kalmemarq.github.io/sudoku")
+    self.setCentralWidget(self.webview)
 
 def main():
   app = QApplication(sys.argv)
--- voxels/main.py
+++ voxels/main.py
@@ -6,6 +6,7 @@ import PIL.Image as Image
 import math
 from utils import perspective
 import numpy as np
+import random
 
 class TextureManager:
   def __init__(self):
@@ -13,14 +14,13 @@ class TextureManager:
 
   def load(self, path: str) -> None:
     im = Image.open(path);
-    imdata = np.frombuffer(im.convert("RGB").tobytes(), np.uint8)
+    imdata = np.frombuffer(im.tobytes(), np.uint8)
 
     txr_id = glGenTextures(1)
-    print(type(txr_id))
     glBindTexture(GL_TEXTURE_2D, txr_id)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, im.width, im.height, 0, GL_RGB, GL_UNSIGNED_BYTE, imdata)
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, im.width, im.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, imdata)
     glBindTexture(GL_TEXTURE_2D, 0)
     self.textures[path] = txr_id
 
@@ -63,36 +63,42 @@ class TileType:
     self.west_txr = west_txr
     self.east_txr = east_txr
 
+
 TILE_TYPES[1] = TileType(1, 2, 0, 0, 0, 0)
 TILE_TYPES[2] = TileType(1, 1, 1, 1, 1, 1)
 TILE_TYPES[3] = TileType(3, 3, 3, 3, 3, 3)
+
+CHUNK_HEIGHT = 32
 
 class Chunk:
   def __init__(self, x: int, z: int):
     self.x = x
     self.z = z
-    self.blocks = np.zeros(16 * 16 * 16, dtype=np.uint8)
+    self.blocks = np.zeros(16 * 16 * CHUNK_HEIGHT, dtype=np.uint8)
     self.list = glGenLists(1)
     self.generate()
 
   def generate(self):
     for x in range(16):
-      for y in range(16):
+      for y in range(CHUNK_HEIGHT):
         for z in range(16):
           if y == 0:
             self.blocks[(y * 16 + z) * 16 + x] = 3
           elif y < 15:
             self.blocks[(y * 16 + z) * 16 + x] = 2
-          else:
+          elif y == 15:
             self.blocks[(y * 16 + z) * 16 + x] = 1
+          else:
+            if random.randrange(0, 200) < 1:
+              self.blocks[(y * 16 + z) * 16 + x] = 3
 
   def set_tile(self, x: int, y: int, z: int, tile_id: int):
-    if x < 0 or x >= 16 or y < 0 or y >= 16 or z < 0 or z >= 16:
+    if x < 0 or x >= 16 or y < 0 or y >= CHUNK_HEIGHT or z < 0 or z >= 16:
       return
     self.blocks[(y * 16 + z) * 16 + x] = tile_id
 
   def get_tile(self, x: int, y: int, z: int):
-    if x < 0 or x >= 16 or y < 0 or y >= 16 or z < 0 or z >= 16:
+    if x < 0 or x >= 16 or y < 0 or y >= CHUNK_HEIGHT or z < 0 or z >= 16:
       return 0
     return self.blocks[(y * 16 + z) * 16 + x]
 
@@ -103,7 +109,7 @@ class Chunk:
 
     glBegin(GL_QUADS)
 
-    for y in range(16):
+    for y in range(CHUNK_HEIGHT):
       for x in range(16):
         for z in range(16):
           bx = x + self.x * 16
@@ -261,16 +267,16 @@ class World:
   def set_tile(self, x: int, y: int, z: int, tile_id: int):
     cx = x // 16
     cz = z // 16
-    if cx < 0 or cx >= self.x_chunks or cz < 0 or cz >= self.z_chunks or y < 0 or y >= 16:
+    if cx < 0 or cx >= self.x_chunks or cz < 0 or cz >= self.z_chunks or y < 0 or y >= CHUNK_HEIGHT:
       return
     self.chunks[cz * 2 + cx].set_tile(x % 16, y % 16, z % 16, tile_id)
 
   def get_tile(self, x: int, y: int, z: int):
     cx = x // 16
     cz = z // 16
-    if cx < 0 or cx >= self.x_chunks or cz < 0 or cz >= self.z_chunks or y < 0 or y >= 16:
+    if cx < 0 or cx >= self.x_chunks or cz < 0 or cz >= self.z_chunks or y < 0 or y >= CHUNK_HEIGHT:
       return 0
-    return self.chunks[cz * 2 + cx].get_tile(x % 16, y % 16, z % 16)
+    return self.chunks[cz * 2 + cx].get_tile(x % 16, y % CHUNK_HEIGHT, z % 16)
 
   def render(self):
     for chunk in self.chunks:
@@ -285,6 +291,8 @@ class Game:
     if not glfw.init():
       exit(0)
 
+    self.chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.!  "
+
     glfw.default_window_hints()
     self.window = glfw.create_window(800, 600, "Voxels", None, None)
     imicon = Image.open("icon.png")
@@ -294,7 +302,8 @@ class Game:
 
     self.texture_manager = TextureManager()
     self.texture_manager.load('grass.png')
-
+    self.texture_manager.load('gui.png')
+    self.current_fps = 0
     self.mouse = {
       'x': 0,
       'y': 0,
@@ -359,6 +368,7 @@ class Game:
       now = glfw.get_time()
       if now - last_time > 1.0:
         last_time = now
+        self.current_fps = frame_counter
         glfw.set_window_title(self.window, f"Voxels {frame_counter} FPS Player({self.player.x},{self.player.y},{self.player.z})")
         frame_counter = 0
 
@@ -382,6 +392,82 @@ class Game:
 
     glBindTexture(GL_TEXTURE_2D, self.texture_manager.get("grass.png"))
     self.world.render()
+
+    glClear(GL_DEPTH_BUFFER_BIT)
+
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    glOrtho(0, 400, 300, 0, 1000.0, 3000.0)
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+    glTranslatef(0.0, 0.0, -2000.0)
+
+    glEnable(GL_BLEND)
+    glBlendFunc(GL_ONE_MINUS_DST_COLOR, GL_ONE_MINUS_SRC_COLOR)
+    self.draw_texture("gui.png", 200 - 8, 150 - 8, 16, 16, 240, 0, 16, 16, 256, 64)
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+    self.draw_texture("gui.png", 200 - 91, 300 - 22, 182, 22, 0, 0, 182, 22, 256, 64)
+    glDisable(GL_BLEND)
+
+    self.draw_text("VOXELS", 1, 1, 0xFFFFFF)
+    self.draw_text(f"{self.current_fps} FPS", 1, 11, 0xFFFFFF)
+
+  def draw_texture(self, texture_name, x: int, y: int, width: int, height: int, u: int, v: int, us: int, vs: int, tw: int, th: int):
+    glBindTexture(GL_TEXTURE_2D, self.texture_manager.get(texture_name))
+
+    u0 = u / tw
+    v0 = v / th
+    u1 = (u + us) / tw
+    v1 = (v + vs) / th
+
+    glColor4f(1.0, 1.0, 1.0, 1.0)
+    glBegin(GL_QUADS)
+    glTexCoord2f(u0, v0)
+    glVertex3f(x, y, 0)
+    glTexCoord2f(u0, v1)
+    glVertex3f(x, y + height, 0)
+    glTexCoord2f(u1, v1)
+    glVertex3f(x + width, y + height, 0)
+    glTexCoord2f(u1, v0)
+    glVertex3f(x + width, y, 0)
+    glEnd()
+
+  def draw_text(self, message: str, x: int, y: int, color: int):
+    glEnable(GL_BLEND)
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+    glBindTexture(GL_TEXTURE_2D, self.texture_manager.get("gui.png"))
+
+    glBegin(GL_QUADS)
+
+    xx = x
+    for char in message:
+      if char == ' ':
+        xx += 8
+        continue
+      
+      idx = self.chars.find(char)
+      u = (idx % 20) * 8
+      v = 48 + (idx // 20) * 8
+
+      u0 = u / 256
+      v0 = v / 64
+      u1 = (u + 8) / 256
+      v1 = (v + 8) / 64
+
+      glTexCoord2f(u0, v0)
+      glVertex3f(xx, y, 0)
+      glTexCoord2f(u0, v1)
+      glVertex3f(xx, y + 8, 0)
+      glTexCoord2f(u1, v1)
+      glVertex3f(xx + 8, y + 8, 0)
+      glTexCoord2f(u1, v0)
+      glVertex3f(xx + 8, y, 0)
+
+      xx += 8
+
+    glEnd()
+
+    glDisable(GL_BLEND)
 
   def tick(self):
     xa = 0
