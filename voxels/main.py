1d34b6ac31159dbbbd6af5bf7fe7de915609b63d
KalmeMarq <55203647+KalmeMarq@users.noreply.github.com>
2023-10-22 15:13:21 +0100

    world and chunks

--- voxels/main.py
+++ voxels/main.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import glfw
 from typing import Dict
 from OpenGL.GL import *
@@ -33,7 +34,7 @@ class TextureManager:
 class Player:
   def __init__(self):
     self.x = 0
-    self.y = 0
+    self.y = 17
     self.z = 0
     self.old_x = 0
     self.old_y = 0
@@ -49,7 +50,235 @@ class Player:
       self.rot_x = -90.0
 
     if self.rot_x > 90.0:
-      self.rot_x = 90.0
+      self.rot_x = 90.
+
+TILE_TYPES: Dict[int, TileType] = {}
+
+class TileType:
+  def __init__(self, down_txr: int, up_txr: int, north_txr: int, south_txr: int, west_txr: int, east_txr: int) -> None:
+    self.down_txr = down_txr
+    self.up_txr = up_txr
+    self.north_txr = north_txr
+    self.south_txr = south_txr
+    self.west_txr = west_txr
+    self.east_txr = east_txr
+
+TILE_TYPES[1] = TileType(1, 2, 0, 0, 0, 0)
+TILE_TYPES[2] = TileType(1, 1, 1, 1, 1, 1)
+TILE_TYPES[3] = TileType(3, 3, 3, 3, 3, 3)
+
+class Chunk:
+  def __init__(self, x: int, z: int):
+    self.x = x
+    self.z = z
+    self.blocks = np.zeros(16 * 16 * 16, dtype=np.uint8)
+    self.list = glGenLists(1)
+    self.generate()
+
+  def generate(self):
+    for x in range(16):
+      for y in range(16):
+        for z in range(16):
+          if y == 0:
+            self.blocks[(y * 16 + z) * 16 + x] = 3
+          elif y < 15:
+            self.blocks[(y * 16 + z) * 16 + x] = 2
+          else:
+            self.blocks[(y * 16 + z) * 16 + x] = 1
+
+  def set_tile(self, x: int, y: int, z: int, tile_id: int):
+    if x < 0 or x >= 16 or y < 0 or y >= 16 or z < 0 or z >= 16:
+      return
+    self.blocks[(y * 16 + z) * 16 + x] = tile_id
+
+  def get_tile(self, x: int, y: int, z: int):
+    if x < 0 or x >= 16 or y < 0 or y >= 16 or z < 0 or z >= 16:
+      return 0
+    return self.blocks[(y * 16 + z) * 16 + x]
+
+  def rebuild_geometry(self, world: World, texture_manager: TextureManager):
+    glNewList(self.list, GL_COMPILE)
+    glEnable(GL_TEXTURE_2D)
+    glBindTexture(GL_TEXTURE_2D, texture_manager.get("grass.png"))
+
+    glBegin(GL_QUADS)
+
+    for y in range(16):
+      for x in range(16):
+        for z in range(16):
+          bx = x + self.x * 16
+          by = y
+          bz = z + self.z * 16
+
+          tile = world.get_tile(x, y, z)
+          if tile < 1:
+            continue
+
+          tile_type = TILE_TYPES[tile]
+
+          x0 = bx
+          y0 = by
+          z0 = bz
+          x1 = bx + 1.0
+          y1 = by + 1.0
+          z1 = bz + 1.0
+
+          if world.get_tile(bx, by - 1, bz) == 0:
+            u = (tile_type.down_txr % 2) * 16
+            v = tile_type.down_txr // 2 * 16
+
+            u0 = u / 32.0
+            v0 = v / 32.0
+            u1 = (u + 16) / 32.0
+            v1 = (v + 16) / 32.0
+
+            glColor4f(0.6, 0.6, 0.6, 1.0)
+            glTexCoord2f(u0, v1)
+            glVertex3f(x0, y0, z1)
+            glTexCoord2f(u0, v0)
+            glVertex3f(x0, y0, z0)
+            glTexCoord2f(u1, v0)
+            glVertex3f(x1, y0, z0)
+            glTexCoord2f(u1, v1)
+            glVertex3f(x1, y0, z1)
+
+          if world.get_tile(bx, by + 1, bz) == 0:
+            u = (tile_type.up_txr % 2) * 16
+            v = tile_type.up_txr // 2 * 16
+
+            u0 = u / 32.0
+            v0 = v / 32.0
+            u1 = (u + 16) / 32.0
+            v1 = (v + 16) / 32.0
+
+            glColor4f(1.0, 1.0, 1.0, 1.0)
+            glTexCoord2f(u1, v1)
+            glVertex3f(x1, y1, z1)
+            glTexCoord2f(u1, v0)
+            glVertex3f(x1, y1, z0)
+            glTexCoord2f(u0, v0)
+            glVertex3f(x0, y1, z0)
+            glTexCoord2f(u0, v1)
+            glVertex3f(x0, y1, z1) 
+
+          if world.get_tile(bx, by, bz - 1) == 0:
+            u = (tile_type.north_txr % 2) * 16
+            v = tile_type.north_txr // 2 * 16
+
+            u0 = u / 32.0
+            v0 = v / 32.0
+            u1 = (u + 16) / 32.0
+            v1 = (v + 16) / 32.0
+            
+            glColor4f(0.6, 0.6, 0.6, 1.0)
+            glTexCoord2f(u1, v0)
+            glVertex3f(x0, y1, z0)
+            glTexCoord2f(u0, v0)
+            glVertex3f(x1, y1, z0)
+            glTexCoord2f(u0, v1)
+            glVertex3f(x1, y0, z0)
+            glTexCoord2f(u1, v1)
+            glVertex3f(x0, y0, z0)
+
+          if world.get_tile(bx, by, bz + 1) == 0:
+            u = (tile_type.south_txr % 2) * 16
+            v = tile_type.south_txr // 2 * 16
+
+            u0 = u / 32.0
+            v0 = v / 32.0
+            u1 = (u + 16) / 32.0
+            v1 = (v + 16) / 32.0
+
+            glColor4f(0.6, 0.6, 0.6, 1.0)
+            glTexCoord2f(u0, v0)
+            glVertex3f(x0, y1, z1)
+            glTexCoord2f(u0, v1)
+            glVertex3f(x0, y0, z1)
+            glTexCoord2f(u1, v1)
+            glVertex3f(x1, y0, z1)
+            glTexCoord2f(u1, v0)
+            glVertex3f(x1, y1, z1)
+
+          if world.get_tile(bx - 1, by, bz) == 0:
+            u = (tile_type.west_txr % 2) * 16
+            v = tile_type.west_txr // 2 * 16
+
+            u0 = u / 32.0
+            v0 = v / 32.0
+            u1 = (u + 16) / 32.0
+            v1 = (v + 16) / 32.0
+
+            glColor4f(0.8, 0.8, 0.8, 1.0)
+            glTexCoord2f(u1, v0)
+            glVertex3f(x0, y1, z1)
+            glTexCoord2f(u0, v0)
+            glVertex3f(x0, y1, z0)
+            glTexCoord2f(u0, v1)
+            glVertex3f(x0, y0, z0)
+            glTexCoord2f(u1, v1)
+            glVertex3f(x0, y0, z1)
+
+          if world.get_tile(bx + 1, by, bz) == 0:
+            u = (tile_type.east_txr % 2) * 16
+            v = tile_type.east_txr // 2 * 16
+
+            u0 = u / 32.0
+            v0 = v / 32.0
+            u1 = (u + 16) / 32.0
+            v1 = (v + 16) / 32.0
+
+            glColor4f(0.8, 0.8, 0.8, 1.0)
+            glTexCoord2f(u0, v1)
+            glVertex3f(x1, y0, z1)
+            glTexCoord2f(u1, v1)
+            glVertex3f(x1, y0, z0)
+            glTexCoord2f(u1, v0)
+            glVertex3f(x1, y1, z0)
+            glTexCoord2f(u0, v0)
+            glVertex3f(x1, y1, z1)
+          
+    glEnd()
+    glEndList()
+
+  def render(self):
+    glCallList(self.list)
+
+  def dispose(self):
+    glDeleteLists(self.list, 1)
+
+class World:
+  def __init__(self, game: Game):
+    self.x_chunks = 4
+    self.z_chunks = 4
+    self.chunks: list[Chunk] = []
+    for x in range(0, self.x_chunks):
+      for z in range(0, self.z_chunks):
+        self.chunks.append(Chunk(x, z))
+
+    for chunk in self.chunks:
+      chunk.rebuild_geometry(self, game.texture_manager)
+
+  def set_tile(self, x: int, y: int, z: int, tile_id: int):
+    cx = x // 16
+    cz = z // 16
+    if cx < 0 or cx >= self.x_chunks or cz < 0 or cz >= self.z_chunks or y < 0 or y >= 16:
+      return
+    self.chunks[cz * 2 + cx].set_tile(x % 16, y % 16, z % 16, tile_id)
+
+  def get_tile(self, x: int, y: int, z: int):
+    cx = x // 16
+    cz = z // 16
+    if cx < 0 or cx >= self.x_chunks or cz < 0 or cz >= self.z_chunks or y < 0 or y >= 16:
+      return 0
+    return self.chunks[cz * 2 + cx].get_tile(x % 16, y % 16, z % 16)
+
+  def render(self):
+    for chunk in self.chunks:
+      chunk.render()
+
+  def dispose(self):
+    for chunk in self.chunks:
+      chunk.dispose()
 
 class Game:
   def __init__(self):
@@ -79,17 +308,7 @@ class Game:
     glfw.set_cursor_pos_callback(self.window, lambda _,xpos,ypos : self.on_cursor_pos(xpos, ypos))
     glfw.set_mouse_button_callback(self.window, lambda _,button,action,mods : self.on_mouse_button(button, action, mods))
 
-    self.tiles = np.zeros(16 * 16 * 16, dtype=np.uint8)
-
-    for x in range(16):
-      for y in range(16):
-        for z in range(16):
-          self.tiles[(y * 16 + z) * 16 + x] = 1
-
-  def get_tile(self, x: int, y: int, z: int):
-    if x < 0 or x >= 16 or y < 0 or y >= 16 or z < 0 or z >= 16:
-      return 0
-    return self.tiles[(y * 16 + z) * 16 + x]
+    self.world = World(self)
 
   def on_mouse_button(self, button, action, mods):
     pass
@@ -111,9 +330,6 @@ class Game:
     self.proj_mat = perspective(1.22172, 800.0 / 600.0, 0.05, 1000.0)
     tick_last_time = glfw.get_time()
     glfw.set_input_mode(self.window, glfw.CURSOR, glfw.CURSOR_DISABLED)
-
-    glEnable(GL_TEXTURE_2D)
-    glEnable(GL_DEPTH_TEST)
 
     while self.running:
       if glfw.window_should_close(self.window):
@@ -146,6 +362,7 @@ class Game:
         glfw.set_window_title(self.window, f"Voxels {frame_counter} FPS Player({self.player.x},{self.player.y},{self.player.z})")
         frame_counter = 0
 
+    self.world.dispose()
     self.texture_manager.dispose()
     glfw.terminate()
 
@@ -160,87 +377,11 @@ class Game:
     glTranslatef(-self.player.x, -self.player.y, -self.player.z)
 
     glEnable(GL_TEXTURE_2D)
+    glEnable(GL_DEPTH_TEST)
+    glEnable(GL_CULL_FACE)
 
     glBindTexture(GL_TEXTURE_2D, self.texture_manager.get("grass.png"))
-
-    for y in range(16):
-      for x in range(16):
-        for z in range(16):
-          if y == 0:
-            u0 = 16.0 / 32.0
-            v0 = 16.0 / 32.0
-            u1 = 32.0 / 32.0
-            v1 = 32.0 / 32.0
-          else:
-            u0 = 0
-            v0 = 0 
-            u1 = 16.0 / 32.0
-            v1 = 16.0 / 32.0 
-
-          x0 = x
-          y0 = y
-          z0 = z
-          x1 = x + 1.0
-          y1 = y + 1.0
-          z1 = z + 1.0
-          glBegin(GL_QUADS)
-
-          if self.get_tile(x, y - 1, z) == 0:
-            glColor4f(0.6, 0.6, 0.6, 1.0)
-            glTexCoord2f(u0, v1)
-            glVertex3f(x0, y0, z1)
-            glTexCoord2f(u0, v0)
-            glVertex3f(x0, y0, z0)
-            glTexCoord2f(u1, v0)
-            glVertex3f(x1, y0, z0)
-            glTexCoord2f(u1, v1)
-            glVertex3f(x1, y0, z1)
-
-          if self.get_tile(x, y + 1, z) == 0:
-            glColor4f(1.0, 1.0, 1.0, 1.0)
-            glTexCoord2f(u1, v1)
-            glVertex3f(x1, y1, z1)
-            glTexCoord2f(u1, v0)
-            glVertex3f(x1, y1, z0)
-            glTexCoord2f(u0, v0)
-            glVertex3f(x0, y1, z0)
-            glTexCoord2f(u0, v1)
-            glVertex3f(x0, y1, z1) 
-
-          if self.get_tile(x, y, z + 1) == 0:
-            glColor4f(0.6, 0.6, 0.6, 1.0)
-            glTexCoord2f(u1, v1)
-            glVertex3f(x0, y0, z0)
-            glTexCoord2f(u1, v0)
-            glVertex3f(x0, y1, z0)
-            glTexCoord2f(u0, v0)
-            glVertex3f(x1, y1, z0)
-            glTexCoord2f(u0, v1)
-            glVertex3f(x1, y0, z0)
-
-          if self.get_tile(x - 1, y, z) == 0:
-            glColor4f(0.8, 0.8, 0.8, 1.0)
-            glTexCoord2f(u1, v0)
-            glVertex3f(x0, y1, z1)
-            glTexCoord2f(u0, v0)
-            glVertex3f(x0, y1, z0)
-            glTexCoord2f(u0, v1)
-            glVertex3f(x0, y0, z0)
-            glTexCoord2f(u1, v1)
-            glVertex3f(x0, y0, z1)
-
-          if self.get_tile(x + 1, y, z) == 0:
-            glColor4f(0.8, 0.8, 0.8, 1.0)
-            glTexCoord2f(u0, v1)
-            glVertex3f(x1, y0, z1)
-            glTexCoord2f(u1, v1)
-            glVertex3f(x1, y0, z0)
-            glTexCoord2f(u1, v0)
-            glVertex3f(x1, y1, z0)
-            glTexCoord2f(u0, v0)
-            glVertex3f(x1, y1, z1)
-          
-          glEnd()
+    self.world.render()
 
   def tick(self):
     xa = 0
